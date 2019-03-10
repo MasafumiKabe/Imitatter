bc351b72b154510a52a35d79de6a180650c51c18
masafumi.kabe <msfmkb@gmail.com>
2019-03-10 19:42:40 +0000

    add image to tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -39,7 +39,7 @@ class TweetsController < ApplicationController
   private
   
   def tweet_params
-    params.require(:tweet).permit(:content)
+    params.require(:tweet).permit(:content, :image)
   end
   
   def correct_user
